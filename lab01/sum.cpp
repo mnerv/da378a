9c1cb7a19151bc40fae9525c5b4e52b3942d99a0
mononerv <me@mononerv.dev>
2022-09-01 23:58:25 +0200

    Lab01: Task04 - Simple sum program

---
+++ lab01/sum.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file   sum.cpp
+ * @author Pratchaya Khansomboon (me@mononerv.dev)
+ * @brief  Task 04 - 07. Calculator and streams redirections
+ *         Linux/macOS (clang)
+ *         clang++ -std=c++20 -Wall -Wextra -Wconversion -Wpedantic -Wshadow -Werror sum.cpp -o sum
+ *
+ *         Windows
+ *         cl /std:c++20 /W4 /WX /EHsc sum.cpp
+ *
+ *         04. Simple sum program
+ *             Usage: ./sum
+ *
+ *             Enter a number and then press enter.
+ *             Use ctrl+d to exit on Unix-based machines or ctrl+x on Windows.
+ *
+ *             The program will output the sum result on the last line.
+ * @date   2022-09-01
+ */
+#include <cstdint>
+#include <iostream>
+
+auto main([[maybe_unused]]int argc, [[maybe_unused]]char const* argv[]) -> int {
+    std::int32_t sum = 0;
+    std::int32_t in  = 0;
+    while (std::cin >> in) sum += in;
+    std::cout << sum << "\n";;
+    return 0;
+}
+
