9dd607766633fdef65645d028c863a104060de9f
mononerv <me@mononerv.dev>
2022-09-02 11:41:20 +0200

    Lab01: Task11 - Print out eval when there's at least one root

--- lab01/poly/polysolver.cpp
+++ lab01/poly/polysolver.cpp
@@ -43,7 +43,13 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char const* argv[])
             Poly2 poly(coeffs[0], coeffs[1], coeffs[2]);
             poly.findRoots(root_count, x1, x2);
             std::cout << "roots: " << root_count << "\n";
-            std::cout << "   x1: " << x1 << ", x2: " << x2 << "\n";
+            std::cout << "   x1: " << x1 << ", x2: " << x2 << ", ";
+            if (root_count == 1) {
+                std::cout << "f(x1) = " << poly.eval(x1) << "\n";
+            } else if (root_count == 2) {
+                std::cout << "f(x1) = " << poly.eval(x1) << ", ";
+                std::cout << "f(x2) = " << poly.eval(x1) << "\n";
+            }
         }
     }
     return 0;
