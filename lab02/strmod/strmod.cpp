03312683635bbcb1386c1059f23d973aac8477fb
mononerv <me@mononerv.dev>
2022-09-09 05:35:49 +0200

    Lab02: Task03 - Fix typos and use std::numeric_limits for SIZE_MAX

--- lab02/strmod/strmod.cpp
+++ lab02/strmod/strmod.cpp
@@ -1,11 +1,11 @@
 /**
  * @file   strmani.cpp
  * @author Pratchaya Khansomboon (me@mononerv.dev)
- * @brief  Task 02 - 04. String manipulation
+ * @brief  Task 02 - 03. String manipulation
  *
- *         02. Substitude string using std::string.
+ *         02. Substitute string using std::string.
  *
- *         03. Substitude string using char* (C-style string manipulation)
+ *         03. Substitute string using char* (C-style string manipulation)
  *
  * @date   2022-09-05
  *
@@ -13,10 +13,11 @@
  */
 #include <iostream>
 #include <string>
+#include <limits>
 
 #include <cstring>
 
-auto substitude_str(std::string& iostr, std::string const& before, std::string const& after) -> void {
+auto substitute_str(std::string& iostr, std::string const& before, std::string const& after) -> void {
     auto match = iostr.find(before);
     while (match != std::string::npos) {
         iostr.replace(match, before.size(), after);
@@ -24,15 +25,14 @@ auto substitude_str(std::string& iostr, std::string const& before, std::string c
     }
 }
 
-constexpr auto operator "" _z(unsigned long long n) -> std::size_t { return n; }
-
-auto substitude_cstr(char const* input, char const* before, char const* after) -> char* {
+auto substitute_cstr(char const* input, char const* before, char const* after) -> char* {
+    static constexpr auto NPOS = std::numeric_limits<std::size_t>::max();
     auto find_str = [](char const* str, char const* match) {
-        std::size_t pos = SIZE_T_MAX;
+        std::size_t pos = NPOS;
         auto const str_len   = std::strlen(str);
         auto const match_len = std::strlen(match);
         for (std::size_t i = 0; i < str_len; ++i) {
-            auto is_match = std::all_of(match, match + match_len, [&, j = 0_z](auto const& c) mutable {
+            auto is_match = std::all_of(match, match + match_len, [&, j = std::size_t(0)](auto const& c) mutable {
                 return i + j < str_len && str[i + (j++)] == c;
             });
             if (is_match) {
@@ -51,7 +51,7 @@ auto substitude_cstr(char const* input, char const* before, char const* after) -
     auto const before_len = std::strlen(before);
     auto const after_len  = std::strlen(after);
 
-    while (match != SIZE_T_MAX) {
+    while (match != NPOS) {
         // Store the previous allocated string for later copy and cleanup
         auto tmp_str = str;
         auto tmp_len = str_len;
@@ -63,7 +63,7 @@ auto substitude_cstr(char const* input, char const* before, char const* after) -
         // Memory copy to the new destination
         //   1. Copy first part before match
         //   2. Copy after string to the new location
-        //   3. Copy rest of the previous string data.
+        //   3. Copy rest of the previous string data include null termination.
         std::memcpy(str, tmp_str, match);
         std::memcpy(str + match, after, after_len);
         std::memcpy(str + match + after_len, tmp_str + match + before_len, tmp_len - match - before_len + 1);
@@ -77,17 +77,13 @@ auto substitude_cstr(char const* input, char const* before, char const* after) -
 
 
 auto main([[maybe_unused]]int argc, [[maybe_unused]]char const* argv[]) -> int {
-    std::string text = "Hello, World!, World, Hej, Wor";
-    std::string out = text;
-    substitude_str(out, "Wor", "");
-    std::cout << "Task02: ";
-    std::cout << out << "\n";
-
-    auto str = substitude_cstr(text.c_str(), "World", "");
-    std::cout << "Task03: ";
+    auto str = substitute_cstr("Hello, World!", "World", "Charlie");
     std::cout << str << "\n";
     delete[] str;
 
+    str = substitute_cstr("Hello, World!", ", World!", ".");
+    std::cout << str << "\n";
+    delete[] str;
     return 0;
 }
 
