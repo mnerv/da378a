750998219f4d81573450587302e3d6e7cad41c91
mononerv <me@mononerv.dev>
2022-09-21 05:13:46 +0200

    Lab04: Task01.d - Sort reverse with C style array

--- lab04/sortint.cpp
+++ lab04/sortint.cpp
@@ -5,6 +5,12 @@
  *
  *         a. Generate random numbers into std::vector and sort it with std::sort.
  *            Print out the results between the steps.
+ *
+ *         b. Random numbers and sort with C style array
+ *
+ *         c. Reverse sort the numbers with std::vector
+ *
+ *         d. Reverse sort the numbers with C style array
  * @date   2022-09-18
  *
  * @copyright Copyright (c) 2022
@@ -64,14 +70,28 @@ auto main([[maybe_unused]]std::int32_t argc, [[maybe_unused]]char const* argv[])
     // Task 01.c - Sort integers in reverse order (std::vector)
     std::vector<std::int32_t> rnums{};
     std::generate_n(std::back_inserter(rnums), MAX_NUM, [&] { return dist(rng); });
-    std::cout << "Random integer numbers (std::vector)\n";
+
+    std::cout << "Random integer numbers reverse (std::vector)\n";
     print_container(std::begin(rnums), std::end(rnums));
     std::cout << "\n";
 
-    std::cout << "Sorted integer numbers (std::vector)\n";
+    std::cout << "Sorted integer numbers reverse (std::vector)\n";
     std::sort(std::rbegin(rnums), std::rend(rnums));
     print_container(std::begin(rnums), std::end(rnums));
     std::cout << "\n\n";
+
+    // Task 01.d - Sort integers reverse with C style array
+    std::int32_t c_rnums[MAX_NUM]{0};
+    std::generate_n(c_rnums, MAX_NUM, [&] { return dist(rng); });
+
+    std::cout << "Random integer numbers reverse (C style array)\n";
+    print_container(c_rnums, c_rnums + MAX_NUM);
+    std::cout << "\n";
+
+    std::cout << "Sorted integer numbers reverse (C style array)\n";
+    std::sort(c_rnums, c_rnums + MAX_NUM, [](auto const& a, auto const& b) { return a > b; });
+    print_container(c_rnums, c_rnums + MAX_NUM);
+    std::cout << "\n";
 
     return 0;
 }
