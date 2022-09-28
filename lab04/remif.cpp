27c66f87d6b2f11898e3adae299006c555c7cd99
mononerv <me@mononerv.dev>
2022-09-28 08:02:37 +0200

    Lab04: Task03 - File added

---
+++ lab04/remif.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file   remif.cpp
+ * @author Pratchaya Khansomboon <me@mononerv.dev>
+ * @brief  Task 3 - Usage of std::remove_if
+ * @date   2022-09-28
+ *
+ * @copyright Copyright (c) 2022
+ */
+#include <cstdint>
+#include <iostream>
+
+auto main([[maybe_unused]]std::int32_t argc, [[maybe_unused]]char const* argv[]) -> std::int32_t {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
