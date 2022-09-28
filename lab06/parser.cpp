d303c310e12dd2d8aa580c22f9ce784e9c21823c
mononerv <me@mononerv.dev>
2022-09-28 19:10:43 +0200

    Lab06: Parser file added

---
+++ lab06/parser.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file   parser.cpp
+ * @author Pratchaya Khansomboon <me@mononerv.dev>
+ * @brief  Parser C@ language.
+ * @date   2022-09-27
+ *
+ * @copyright Copyright (c) 2022
+ */
+#include "parser.hpp"
+
+namespace cat {
+
+}
+
---
+++ lab06/parser.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file   parser.hpp
+ * @author Pratchaya Khansomboon <me@mononerv.dev>
+ * @brief  Parser C@ language.
+ * @date   2022-09-27
+ *
+ * @copyright Copyright (c) 2022
+ */
+#ifndef CAT_PARSER_HPP
+#define CAT_PARSER_HPP
+
+#include "token.hpp"
+#include "lexer.hpp"
+
+namespace cat {
+
+class ast_node {
+};
+
+class parser {
+  public:
+  private:
+};
+
+}
+
+#endif  // CAT_PARSER_HPP
