1c42d4d88dcb9e68212d766fb5fac787268b5276
mononerv <pratchaya.k.git@gmail.com>
2022-10-21 05:01:57 +0200

    Lab06: ParserTest program

---
+++ lab06/tests/parsertest.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file   asttest.cpp
+ * @author Pratchaya Khansomboon <me@mononerv.dev>
+ * @brief  Google Test suites for cat language library.
+ * @date   2022-10-17
+ *
+ * @copyright Copyright (c) 2022
+ */
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "lexer.hpp"
+#include "parser.hpp"
+
+TEST(parser, numeric_literal) {
+    ASSERT_TRUE(false);
+}
+
+auto main(int argc, char const* argv[]) -> int {
+    testing::InitGoogleTest(&argc, (char**)argv);
+    return RUN_ALL_TESTS();
+}
